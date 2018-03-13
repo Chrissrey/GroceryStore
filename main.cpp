1a07fb8877d97d490f8abd5b197524c40321b14e
Rey Almaraz <ChrisRey@wlan-t-195-63-dhcp.CSUChico.EDU>
2018-03-13 11:04:13 -0700

    Created a vetor called grceryList

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 //Author:Christopher rey Almaraz
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 const int LIST_SIZE=5;
 
 int main()
 {
-  string list[5]; //array of 5 strings
+  vector<string> groceryList; //created a vector of strings called groceryList  ///string list[5]; //array of 5 strings called list
   int numItems = 0;
   char input;
   int index;
-  string buffer;
+  string buffer;  //
   
   index=0;
   do
@@ -29,23 +30,30 @@ int main()
 	cout<<"What is the item?\n";
 	cin>>buffer;
 
-	if(index<5)
-	{
-	list[index]=buffer;
-	index++;}
-	else
+				//if(index<5)
+	
+	groceryList.push_back(buffer); // if index is <5 then cin the persons item into the list
+					//index++;
+/*		else
 	{
        		cout<< "You'll need a bigger list!\n";
 	}
-
+*/
  }
 }while( input != 'q' && input != 'Q');
 
+	if(groceryList.size()>0)
 	cout<< "==ITEMS TO BUY==\n";
+	else
+	{ 
+		cout<<"No items to buy.\n";
+		return 0;
+	}
+		
 
-for(int items =0;items<5;items++)
+for(int items =0;items<groceryList.size();items++)
 { 	string Grocery;
-	Grocery=list[items];
+	Grocery=groceryList[items]; //the items in thr groceryList are in the Grocery
 	cout<<items+1<< " " << Grocery<< "\n";
 }
 
